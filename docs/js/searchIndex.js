acb8fab711960554d3369da19c9b59a2a21239a4
krzysztofdarmon <kdmessi@gmail.com>
2021-06-01 16:44:07 +0200

    feat: add phpdocumentor and very first docs

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\App\\Kernel",
+            "name": "Kernel",
+            "summary": "",
+            "url": "classes/App-Kernel.html"
+        },                {
+            "fqsen": "\\App\\Kernel\u003A\u003AregisterBundles\u0028\u0029",
+            "name": "registerBundles",
+            "summary": "",
+            "url": "classes/App-Kernel.html#method_registerBundles"
+        },                {
+            "fqsen": "\\App\\Kernel\u003A\u003AgetProjectDir\u0028\u0029",
+            "name": "getProjectDir",
+            "summary": "",
+            "url": "classes/App-Kernel.html#method_getProjectDir"
+        },                {
+            "fqsen": "\\App\\Kernel\u003A\u003AconfigureContainer\u0028\u0029",
+            "name": "configureContainer",
+            "summary": "",
+            "url": "classes/App-Kernel.html#method_configureContainer"
+        },                {
+            "fqsen": "\\App\\Kernel\u003A\u003AconfigureRoutes\u0028\u0029",
+            "name": "configureRoutes",
+            "summary": "",
+            "url": "classes/App-Kernel.html#method_configureRoutes"
+        },                {
+            "fqsen": "\\App\\Kernel\u003A\u003ACONFIG_EXTS",
+            "name": "CONFIG_EXTS",
+            "summary": "",
+            "url": "classes/App-Kernel.html#constant_CONFIG_EXTS"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\App",
+            "name": "App",
+            "summary": "",
+            "url": "namespaces/app.html"
+        }            ]
+);
