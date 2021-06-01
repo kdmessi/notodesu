b53dc8810be950ae3ea9e8343a5e372590342b90
krzysztofdarmon <kdmessi@gmail.com>
2021-06-01 18:32:30 +0200

    feat: add translations and make switch between pl and en language

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * DefaultController
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/{_locale<%app.locales%>}", name="homepage")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="default")
+     * @return Response
+     */
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'pl']);
+    }
+}
